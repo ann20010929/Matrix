7dc33075b7dc86132ff1174b1b35fd3f3378f1e9
ann20010929 <64964377+ann20010929@users.noreply.github.com>
2021-02-24 00:18:46 +0800

    pas encore fini
    pas ok si matrice commence pat m00=0

--- para1/inversible.py
+++ para1/inversible.py
@@ -14,6 +14,7 @@ MI3 = np.array([[5, 8, 1, 1, 0, 0],
                 [6, 7, 1, 0, 1, 0],
                 [2, 5, 9, 0, 0, 1]])
 
+
 def forward_elimination(M):
     nxn = len(M)  # forcement car n*n  n'existe que pour Mn(K)
     for k in range(diag - 1):  # itérate k fois
@@ -33,4 +34,24 @@ def diag_1(M): #on veut que tous les mkk soient = 1
             for i in range(k, len(M)):  # ligne
                 for j in range(len(M[0])):  # colonne
                     M[i][j] *= ratio
+    return M
+
+  
+def Jordan(M):
+    M = diag_1(M)
+    for k in range(len(M)):  # =3
+        for i in range(k, len(M)):  # ligne
+            for j in range(len(M[0])):  # colonne
+                if (i < 3 and j < 3) and i!=j and M[i][j] != 0:
+                        jArr = np.array([j, j])
+                        iArr = np.arange(0, len(M))
+                        zeroIndexArr = np.array(i)
+                        new_iArr = np.setdiff1d(iArr, zeroIndexArr)
+
+                        for c in range(len(jArr)):
+                            mij = M[new_iArr[c]][jArr[c]]
+                            if mij != 0:
+                                ratioForLrow = -(M[i][j] / M[j][j])
+                                M[i, :] = M[i, :] + ratioForLrow * M[j, :]
+                                break
     return M
